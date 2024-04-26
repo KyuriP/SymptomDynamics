7a47729d9e1eec8cde9fa177abcf013d13399d85
Kyuri <kyurheep@gmail.com>
2024-04-26 10:44:24 +0200

    cleaning

--- code/data_network.R
+++ code/data_network.R
@@ -4,7 +4,10 @@ library(dplyr)
 library(stringr)
 
 helius2 <- read_sav("data/HELIUS_itemscores.sav")
-helius2$H2_WlbvRecent9
+helius2$H1_WlbvRecent8
+helius2$H1_WlbvRecent9
+helius2$H1_WlbvRecent_89
+
 
 dep_scores <- helius2 |> 
   select(contains("WlbvRecent") & !ends_with("Ingevuld"), ID) |>
@@ -13,15 +16,15 @@ dep_scores <- helius2 |>
   drop_na() |>
   select(-wave1) %>% 
   filter(!(item_num == "8" | item_num == "9")) |>
-  mutate(item_num = case_when(item_num == "1" ~ "anhedonia",
-            item_num == "2"~ "sadness",
-            item_num == "3"~ "sleep",
-            item_num == "4"~ "energy",
-            item_num == "5"~ "appetite",
-            item_num == "6"~ "guilt",
-            item_num == "7"~ "concentration",
-            item_num == "89" ~ "motor",
-            item_num == "10" ~ "suicidal")
+  mutate(item_num = case_when(item_num == "1" ~ "anh",
+            item_num == "2"~ "sad",
+            item_num == "3"~ "slp",
+            item_num == "4"~ "ene",
+            item_num == "5"~ "app",
+            item_num == "6"~ "glt",
+            item_num == "7"~ "con",
+            item_num == "89" ~ "mot",
+            item_num == "10" ~ "sui")
             )
 
 dep_list <- dep_scores %>% 
@@ -29,18 +32,85 @@ dep_list <- dep_scores %>%
   {setNames(group_split(.), group_keys(.)[[1]])}  %>% 
   map(~.x |>pivot_wider(id_cols = ID, names_from = item_num, values_from = value))
   
-
+# each wave separately
 net_list <- dep_list |>
-  map(~.x |>select(-ID) |>
+  map(~.x |>select(-ID) |> relocate(sui, .after = mot) |>
       estimateNetwork(.x, default = "EBICglasso") |>
         plot(.x)
       )
 
+Layout_simnet <- totavgnetwork2$layout
 
-plot(net_list$H1)
+net_list$H1$layout <- Layout_simnet
 
-# H1net <- estimateNetwork(H1, default = "EBICglasso")
-# plot(H1net)
+ plot(net_list$H1)
+
+res_netH1_helius <- centrality_auto(net_list$H1)
+rownames(res_netH1_helius$node.centrality) <- colnames(A)
+
+cent_netH1_helius <- res_netH1_helius$node.centrality$Strength |>as_data_frame() |>  mutate(node = factor(rownames(res_netH1_helius$node.centrality), levels= c(rownames(res_netH1_helius$node.centrality)))
+)
+
+cent_netH1_helius$dummyB <- "Strength"
+cent_netH1_helius |>
+  ggplot(aes(x=value, y = node, group=1)) + 
+  geom_point(color = "deepskyblue4") + 
+  geom_path(color = "deepskyblue4") + 
+  theme_bw() +
+  labs(x = "", y = "", color = "") +
+  theme(text=element_text(size=15))+
+  facet_grid(. ~ dummyB)
+
+
+# each wave separately
+average_net_allwaves <- dep_list |>
+  map(~.x |>select(-ID) |> relocate(sui, .after = mot)
+  ) |> list_rbind() |>
+  drop_na()
+
+totavgnetwork_helius <- estimateNetwork(average_net_allwaves, default = "EBICglasso")
+totavgnetwork_helius2 <- plot(totavgnetwork_helius)
+totavgnetwork_helius2$layout <- Layout_simnet
+plot(totavgnetwork_helius2)
+
+# weighted adj mat
+mat <- round(totavgnetwork_helius$graph, 2)
+ifelse(mat < 0.1, 0, mat)
+round(totavgnetwork$graph, 2)
+
+res_totavg_helius <- centrality_auto(totavgnetwork_helius2)
+rownames(res_totavg_helius$node.centrality) <- colnames(A)
+
+cent_totavg_helius <- res_totavg_helius$node.centrality$Strength |>as_data_frame() |>  mutate(node = factor(rownames(res_totavg_helius$node.centrality), levels= c(rownames(res_totavg_helius$node.centrality)))
+)
+
+cent_totavg_helius$dummyB <- "Strength"
+cent_totavg_helius_plot <- cent_totavg_helius |>
+  ggplot(aes(x=value, y = node, group=1)) + 
+  geom_point(color = "deepskyblue4") + 
+  geom_path(color = "deepskyblue4") + 
+  theme_bw() +
+  labs(x = "", y = "", color = "") +
+  theme(text=element_text(size=15))+
+  facet_grid(. ~ dummyB)
+
+# combine two centralities
+cent_totavg$grp <- "toy"
+cent_totavg_helius$grp <- "helius"
+both_cents <- cent_totavg |> full_join(cent_totavg_helius)
+# normalization
+maxval <- max(both_cents$value)
+minval <- min(both_cents$value)
+both_cents$val <- both_cents$value - minval / (maxval - minval)
+
+cent_combined <- both_cents |>
+  ggplot(aes(x=value, y = node, group=grp, color = grp)) + 
+  geom_point() + 
+  geom_path() + 
+  theme_bw() +
+  labs(x = "", y = "", color = "") +
+  theme(text=element_text(size=15))+
+  facet_grid(. ~ dummyB)
 
 
 ### ==========================
