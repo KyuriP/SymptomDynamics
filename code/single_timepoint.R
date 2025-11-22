2526b65888eb841493937cb5e036fd029861752d
Kyuri <kyurheep@gmail.com>
2025-11-22 21:22:04 +0100

    remove med level - single time point fig

--- code/single_timepoint.R
+++ code/single_timepoint.R
@@ -20,7 +20,7 @@ high <- readRDS("data/aggregated_res.rds")
 # Representative time points for each phase
 t_before <- 500
 t_during <- 1500
-t_after  <- 4000
+t_after  <- 3000
 
 # --- Helper functions -------------------------------------------------------
 
@@ -48,9 +48,9 @@ low_before  <- get_phase_data(low,  t_before)
 low_during  <- get_phase_data(low,  t_during)
 low_after   <- get_phase_data(low,  t_after)
 
-med_before  <- get_phase_data(med,  t_before)
-med_during  <- get_phase_data(med,  t_during)
-med_after   <- get_phase_data(med,  t_after)
+# med_before  <- get_phase_data(med,  t_before)
+# med_during  <- get_phase_data(med,  t_during)
+# med_after   <- get_phase_data(med,  t_after)
 
 high_before <- get_phase_data(high, t_before)
 high_during <- get_phase_data(high, t_during)
@@ -61,9 +61,9 @@ low_before_net  <- estimate_single_GGM(low_before)
 low_during_net  <- estimate_single_GGM(low_during)
 low_after_net   <- estimate_single_GGM(low_after)
 
-med_before_net  <- estimate_single_GGM(med_before)
-med_during_net  <- estimate_single_GGM(med_during)
-med_after_net   <- estimate_single_GGM(med_after)
+# med_before_net  <- estimate_single_GGM(med_before)
+# med_during_net  <- estimate_single_GGM(med_during)
+# med_after_net   <- estimate_single_GGM(med_after)
 
 high_before_net <- estimate_single_GGM(high_before)
 high_during_net <- estimate_single_GGM(high_during)
@@ -76,13 +76,13 @@ net_layout <- readRDS("data/net_layout.rds")
 
 
 cols <- c("Low resilience"    = "#CC3300",   # red
-          "Medium resilience" = "#3366CC",   # blue
+          #"Medium resilience" = "#3366CC",   # blue
           "High resilience"   = "#339966")   # green
 
 # Determine consistent edge scaling
 max_value_all <- max(
   abs(low_before_net$graph), abs(low_during_net$graph), abs(low_after_net$graph),
-  abs(med_before_net$graph), abs(med_during_net$graph), abs(med_after_net$graph),
+  #abs(med_before_net$graph), abs(med_during_net$graph), abs(med_after_net$graph),
   abs(high_before_net$graph), abs(high_during_net$graph), abs(high_after_net$graph)
 )
 
@@ -106,12 +106,17 @@ plot_net <- function(net, color) {
 }
 
 # --- Create figure ----------------------------------------------------------
-png("figures/single_time_networks.png", width = 600, height = 1600, res = 300)
+# png("figures/single_time_networks.png", width = 600, height = 1600, res = 300)
+# png("figures/single_time_networks_2level.png", width = 600, height = 600, res = 600)
 
-par(mfrow = c(3, 3),
-    mar = c(1, 1, 1, 1),  # internal margins inside each panel
-    oma = c(8, 5, 5, 5))  # outer margins around the grid
 
+# three by three fig
+# par(mfrow = c(3, 3),
+#     mar = c(1, 1, 1, 1),  # internal margins inside each panel
+#     oma = c(8, 5, 5, 5))  # outer margins around the grid
+
+# two by three fig
+par(mfrow=c(2,3))
 # ------------------ Plot all 9 networks ------------------
 
 # Row 1 – Low resilience
@@ -120,14 +125,16 @@ plot_net(low_during_net,  cols["Low resilience"])
 plot_net(low_after_net,   cols["Low resilience"])
 
 # Row 2 – Medium resilience
-plot_net(med_before_net,  cols["Medium resilience"])
-plot_net(med_during_net,  cols["Medium resilience"])
-plot_net(med_after_net,   cols["Medium resilience"])
+# plot_net(med_before_net,  cols["Medium resilience"])
+# plot_net(med_during_net,  cols["Medium resilience"])
+# plot_net(med_after_net,   cols["Medium resilience"])
 
 # Row 3 – High resilience
 plot_net(high_before_net, cols["High resilience"])
 plot_net(high_during_net, cols["High resilience"])
 plot_net(high_after_net,  cols["High resilience"])
+
+
 
 # --- Common legend ----------------------------------------------------------
 par(fig = c(0, 1, 0, 0.12), new = TRUE, mar = c(0, 0, 0, 0))  # ⬅ more vertical space
@@ -142,5 +149,6 @@ legend("bottom",
        pt.cex = 5,      # larger color boxes
        text.col = "black",
        xpd = TRUE,
-       y.intersp = 5)  # increase line spacing
+       y.intersp = 0)  # increase line spacing
 
+# dev.off()
